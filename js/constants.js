fb2a12be24e098bfbe576c8c341eb8506f05491a
yavtuh <yavtushenko2018seriy@gmail.com>
2023-03-29 16:55:38 +0300

    module-task4

--- js/constants.js
+++ js/constants.js
@@ -78,8 +78,46 @@ export const photosArray = [
 ]
 
 export const typesObject = {
-    palace: "Палац",
-    flat: "Квартира",
-    house: "Будинок",
-    bungalow: "Бунгало"
+    palace: {
+        name:"Палац",
+        minPrice: 10000
+    },
+    flat: {
+        name:"Квартира",
+        minPrice: 1000
+    },
+    house: {
+        name:"Будинок",
+        minPrice: 5000
+    },
+    bungalow: {
+        name:"Бунгало",
+        minPrice: 0
+    }
 };
+
+export const priceInput = {
+    min: 1000
+};
+
+export const roomsGuests = {
+    1:[1],
+    2:[1,2],
+    3:[1,2,3],
+    100:[0]
+}
+
+export const guestsRooms = {
+    0:[100],
+    1:[1],
+    2:[1,2],
+    3:[1,2,3]
+}
+
+// export const timeInTimOut = {
+//     "12:00":
+// }
+
+// export const timeOutTimeIn = {
+
+// }
